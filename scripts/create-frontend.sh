f645e2756e89de2acdd7160639593323ad2f20fa
Guid1111 <tony@DESKTOP-JI2MQBC.localdomain>
2023-08-12 14:06:48 +0100

    script udpates

--- scripts/create-frontend.sh
+++ scripts/create-frontend.sh
@@ -1,1 +1,1 @@
-aws cloudformation create-stack --stack-name frontend --template-body file://..//.circleci/files/frontend.yml  --parameters ParameterKey=ID,ParameterValue=123 --region=us-east-1
+aws cloudformation create-stack --stack-name frontend --template-body file://..//.circleci/files/frontend.yml  --parameters ParameterKey=ID,ParameterValue=067215852314 --region=us-east-1
